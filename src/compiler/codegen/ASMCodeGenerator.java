cb97536d177af3453243b260dde9461f811a4990
Matthew Smith <smithm19@wit.edu>
2015-03-30 14:54:27 -0400

    code gen finished

--- src/compiler/codegen/ASMCodeGenerator.java
+++ src/compiler/codegen/ASMCodeGenerator.java
@@ -5,6 +5,9 @@ import java.util.List;
 import compiler.lexer.ASMLexer;
 import compiler.lexer.Token;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * ASMCodeGenerator will handle the conversion of 
  * Assembly instructions to their proper hexadecimal
@@ -46,30 +49,43 @@ public class ASMCodeGenerator implements CodeGenerator {
 						case "SUB":
 							// Implement the rest of these! the hex conversions can
 							// be derived from the CPU ISA reference in the README.
+							output += "1";
 							break;
 						case "MUL":
+							output += "2";
 							break;
 						case "DIV":
+							output += "3";
 							break;
 						case "AND":
+							output += "4";
 							break;
 						case "OR":
+							output += "5";
 							break;
 						case "XNOR":
+							output += "6";
 							break;
 						case "MOV":
+							output += "7";
 							break;
 						case "JZE":
+							output += "8";
 							break;
 						case "JZN":
+							output += "9";
 							break;
 						case "JZG":
+							output += "A";
 							break;
 						case "JZL":
+							output += "B";
 							break;
 						case "LOAD":
+							output += "C";
 							break;
 						case "STOR":
+							output += "E";
 							break;
 						default:
 							// We shouldn't ever get here, but if we do
@@ -80,13 +96,73 @@ public class ASMCodeGenerator implements CodeGenerator {
 					}
 					break;
 				case ASMLexer.REGISTER:
-					// Do the same this for a register token.
+					// Do the same thing for a register token.
 					// Here all you need is the number at the
 					// end. EX: R0 -> 0.
+				{
+					switch(t.text)
+					{
+					case "R0":
+						output+="0";
+						break;
+					case "R1":
+						output+="1";
+						break;
+					case "R2":
+						output+="2";
+						break;
+					case "R3":
+						output+="3";
+						break;
+					case "R4":
+						output+="4";
+						break;
+					case "R5":
+						output+="5";
+						break;
+					case "R6":
+						output+="6";
+						break;
+					case "R7":
+						output+="7";
+						break;
+					case "R8":
+						output+="8";
+						break;
+					case "R9":
+						output+="9";
+						break;
+					case "RA":
+						output+="A";
+						break;
+					case "RB":
+						output+="B";
+						break;
+					case "RC":
+						output+="C";
+						break;
+					case "RD":
+						output+="D";
+						break;
+					case "RE":
+						output+="E";
+						break;
+					case "RF":
+						output+="F";
+						break;
+					}
+				}
+					
 					break;
 				case ASMLexer.IMMD:
 					// Basically just append the text of this
 					// to the output. We can check this later.
+					Pattern p = Pattern.compile("[0-f]{1,2}");
+					Matcher m = p.matcher(t.text);
+					if(m.matches())
+					{
+						output+=t.text;
+					}
 					break;
 				default:
 					// We shouldn't ever get here, but if we do
