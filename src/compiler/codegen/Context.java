571b9523c2df8b7c904b7f62782c0c6defc195a3
Zachary X Zeigler <zeiglerz1@wit.edu>
2015-04-09 09:05:40 -0400

    context

---
+++ src/compiler/codegen/Context.java
@@ -0,0 +1,67 @@
+package compiler.codegen;
+
+/**
+ * Created by Zach on 4/9/2015.
+ */
+public class Context
+{
+    private Token t1;
+    private Token t2;
+    private Char type;
+    private int count;
+
+    public Context(Token t1, Token t2, Char type)
+    {
+        this.t1 = t1;
+        this.t2 = t2;
+        this.type = type;
+    }
+
+    public Context(Token t1, Token t2, Char type, int count)
+    {
+        this.t1 = t1;
+        this.t2 = t2;
+        this.type = type;
+        this.count = count;
+    }
+
+    public Token getTokOne()
+    {
+        return t1;
+    }
+
+    public Token getTokTwo()
+    {
+        return t2;
+    }
+
+    public Char getType()
+    {
+        return type;
+    }
+
+    public int getCount()
+    {
+        return count;
+    }
+
+    public void setTokOne(Token t1)
+    {
+        this.t1 = t1;
+    }
+
+    public void setTokTwo(Token t2)
+    {
+        this.t2 = t2;
+    }
+
+    public void setType(Char type)
+    {
+        this.type = type;
+    }
+
+    public int setCount(int count)
+    {
+        this.count = count;
+    }
+}
